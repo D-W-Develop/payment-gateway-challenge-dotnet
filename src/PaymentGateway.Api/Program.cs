d77d08f02218cc4c83829f94fcede1c479846b68
D-W-Develop <63415268+D-W-Develop@users.noreply.github.com>
2025-07-09 20:16:32 +0100

    Put URL in config

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -4,13 +4,13 @@ using PaymentGateway.Api.Repository;
 using PaymentGateway.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var abcBankUrl = builder.Configuration.GetValue<string>("ABCBankUrl");
 //Services
 builder.Services
     .AddControllers()
     .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddSingleton<IValidationService, ValidationService>();
-builder.Services.AddHttpClient<IABCBankClient, ABCBankClient>(c => c.BaseAddress = new("http://localhost:8080/"));
+builder.Services.AddHttpClient<IABCBankClient, ABCBankClient>(c => c.BaseAddress = new(abcBankUrl!));
 builder.Services.AddSingleton<IPaymentRecordMappingService, PaymentRecordMappingService>();
 builder.Services.AddSingleton<PaymentRecordRepository>();
 var app = builder.Build();
