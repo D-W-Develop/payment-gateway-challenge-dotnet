cc56d6c24a03a12ab74df1b68b0bf2480bb71441
D-W-Develop <63415268+D-W-Develop@users.noreply.github.com>
2025-07-09 18:57:57 +0100

    Tests

---
+++ test/PaymentGateway.Api.Tests/Repository/PaymentRecordRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using PaymentGateway.Api.Contract;
+using PaymentGateway.Api.PaymentsController.Models;
+using PaymentGateway.Api.Repository;
+
+using Xunit;
+
+namespace PaymentGateway.Api.Tests.Repository;
+public class When_Using_The_Payment_Record_Repository
+{
+    private readonly PaymentRecordRepository _repository = new();
+
+    [Fact]
+    public void Can_Add_And_Retrieve_Records()
+    {
+        var record = new PaymentRecord
+        (
+            Id: Guid.NewGuid(),
+            Status: PaymentStatus.Declined,
+            ExpiryYear: 2026,
+            ExpiryMonth: 11,
+            Amount: 100,
+            CardNumberLastFour: 1111,
+            Currency: "GBP"
+        );
+
+        _repository.AddOrUpdate(record);
+        var result = _repository.TryGet(record.Id);
+        Assert.True(result.Success);
+        Assert.Equal(record, result.Value);
+    }
+
+    [Fact]
+    public void If_Record_Not_Stored_Returns_Failure()
+    {
+        var result = _repository.TryGet(Guid.NewGuid());
+        Assert.False(result.Success);
+    }
+}
---
+++ test/PaymentGateway.Api.Tests/Services/PaymentRecordMappingServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using PaymentGateway.Api.Contract;
+using PaymentGateway.Api.PaymentsController.Models;
+using PaymentGateway.Api.Services;
+
+using Xunit;
+
+namespace PaymentGateway.Api.Tests.Services;
+public class When_Mapping_A_Payment_Request_To_A_Payment_Record
+{
+    private readonly PaymentRecordMappingService _mappingService = new();
+
+    [Fact]
+    public void Should_Map_Values_Correctly_Including_Masking_Credit_Card_Number()
+    {
+        var req = new PaymentRequest
+        (
+
+            ExpiryYear: 2026,
+            ExpiryMonth: 11,
+            Amount: 100,
+            CardNumber: 12345678911111,
+            Currency: "GBP",
+            CVV: 123
+        );
+        var id = Guid.NewGuid();
+        var status = PaymentStatus.Authorized;
+
+        var expectedResult = new PaymentRecord
+        (
+            Id:id,
+            Status:status,
+            ExpiryYear: 2026,
+            ExpiryMonth: 11,
+            Amount: 100,
+            CardNumberLastFour: 1111,
+            Currency: "GBP"
+        );
+        var result = _mappingService.MapToPaymentRecord(req, status, id);
+        Assert.Equal(expectedResult, result);
+    }
+}
